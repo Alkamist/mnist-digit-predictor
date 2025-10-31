eaa398f35150b279e74db9a4b54f7bfb33efe8e5
Alkamist <coreylehman@me.com>
2025-10-31 11:32:44 -0400

    Reset version to 0.1.0 for actual deployment.

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 
-docker build -t alkamist1/digit-image-predictor:server-0.1.4 ./server
-docker build -t alkamist1/digit-image-predictor:worker-0.1.4 ./worker
+docker build -t alkamist1/digit-image-predictor:server-0.1.0 ./server
+docker build -t alkamist1/digit-image-predictor:worker-0.1.0 ./worker
 
 pause
